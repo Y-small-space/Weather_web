2b80bb64d2483b4f19b4feb52d524cb0ec454958
“y-small-space” <jy.small.space@outlook.com>
2023-10-22 21:55:50 +0800

    >

---
+++ weather_web/src/views/layout/layoutContainer.vue
@@ -0,0 +1,27 @@
+<script setup></script>
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside class="aside" width="300px">
+        <el-card> london </el-card>
+      </el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </div>
+</template>
+<style>
+.comment-layout {
+  height: 100%;
+}
+.aside {
+  height: 100%;
+}
+.main {
+  background-color: darkgray;
+}
+.card {
+  height: 90px;
+  width: 260px;
+  margin-top: 20px;
+}
+</style>
